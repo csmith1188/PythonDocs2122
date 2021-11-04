02bf7e9aa4af27cf7e1bf44d9b5d5b7bb06725db
Mr. Smith <csmith@ytech.edu>
2021-11-04 14:39:15 -0400

    import modules

---
+++ 02_importmodules.py
@@ -0,0 +1,36 @@
+#import the basics module to use its code
+import basics
+
+#make a new application object from the basics module
+app = basics.newProgram()
+
+#use a method that belongs to our new application object
+app.print('yo momma')
+
+#print a property of our new application object
+app.print( app.debugging )
+
+#This line won't print if app.debugging is False
+app.debug("Hello")
+
+#We'll enable debugging for the application
+app.debugging = True
+app.debug('Now it works!')
+
+#import a default Python module
+import random
+
+#Use a method from the random module
+randomNumber = random.randint(1, 10)
+app.print(randomNumber)
+
+
+
+
+
+
+
+
+
+
+
